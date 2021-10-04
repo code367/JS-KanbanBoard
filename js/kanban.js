bd0c43b2e64d88f6ccc288726bd370cb0ff69732
Thabang <tsmodisaotsile@gmail.com>
2021-10-04 16:08:13 +0200

    added code

--- js/kanban.js
+++ js/kanban.js
@@ -8,7 +8,7 @@ const add_btn = document.querySelector('.add');
 add_btn.addEventListener('click', () => {
   const target = document.querySelector('#requested');
   if (adding == false) {
-    adding = true;
+    adding = "true";
     target.appendChild(create_item());
   } else {
     error.innerHTML = message;
@@ -16,7 +16,51 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  const item = document.createElement("div");
+  item.classList.add("item");
+
+  item.id = "item-" + order;
+  item.draggable = true;
+
+  item.addEventListener("dragstart", (event) => {
+    return event.dataTransfer.setData("text", event.target.id);
+  });
+
+  item.addEventListener("dragend", (event) => {
+    return event.dataTransfer.clearData();
+  });
+
+  let input = document.createElement("input");
+  item.appendChild(input);
+
+  const save_btn = document.createElement("button");
+  save_btn.innerHTML = "Save";
+
+  save_btn.addEventListener("click", () => {
+    error.innerHTML = "";
+    if (input.value != "") {
+      order += 1;
+      item.innerHTML = input.value;
+      adding = false;
+    } else {
+      error.innerHTML = message;
+    }
+  });
+
+  return item.appendChild(save_btn);
+
+  return item;
+
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(id));
+  });
+  element.addEventListener("dragover", (event) => {
+    event.preventDefault();
+  });
 });
+
